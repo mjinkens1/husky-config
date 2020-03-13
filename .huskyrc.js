2311545af6a97948ff8b01f35ba3d3a2adc93aa4
Matt <>
2020-03-13 13:54:46 -0500

    Change to export config directly

--- .huskyrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  hooks: {
-    "pre-commit":
-      "npm run format && npm run lint-staged && CI=true npm run test --coverage --watchAll=false || exit 0"
-  }
-};
--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-#!/usr/bin/env node
-const { dirname } = require("path");
-const { openSync, writeFileSync } = require("fs");
-const { execSync } = require("child_process");
-const config = require("./config");
-
-const rootPath = dirname(require.main.filename).split("/node_modules")[0];
-const configFilename = ".huskyrc.js";
-const filepath = `${rootPath}/${configFilename}`;
-const fileContent = `module.exports = ${JSON.stringify(config)}`;
-
-console.log("Creating husky config file 🐕");
-
-try {
-  openSync(filepath, "w");
-  writeFileSync(filepath, fileContent);
-} catch (error) {
-  console.log("Oops! Something went wrong while creating the config file.");
-}
-
-try {
-  console.log("Formatting config file...");
-  execSync(`prettier --write ${configFilename}`);
-} catch (error) {
-  console.log("Oops looks like prettier isn't installed for formatting!");
-}
-
-console.log("Done!");
+module.exports = {
+  hooks: {
+    "pre-commit":
+      "npm run format && npm run lint-staged && CI=true npm run test --coverage --watchAll=false || exit 0"
+  }
+};
